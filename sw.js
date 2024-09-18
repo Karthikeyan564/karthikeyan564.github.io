d9e027be65222750a3768069045ed06448c2864e
Karthikeyan564 <Karthikeyan564>
2024-09-17 22:52:49 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-02c4130c47bc12c8ec9b.js"
+    "url": "webpack-runtime-6ea6c42fbb032cf8d5d2.js"
   },
   {
     "url": "framework-025a10830d88a05b2804.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "85503810c6f5842384199c172654d763"
+    "revision": "dae563a7a231d7473cef4b4b5a24adfd"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js"
